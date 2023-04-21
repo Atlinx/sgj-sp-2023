6d36c895143d9bf9a3eeb0114a62347174624a80
Luke Letourneau <Bluestackthingie@gmail.com>
2023-04-21 18:53:11 -0400

    Score almost done

--- Scripts/General/GameManager.cs
+++ Scripts/General/GameManager.cs
@@ -14,9 +14,7 @@ namespace Game
         [Export]
         public ulong Seed { get; set; }
 
-        [Export] public int WhiskTickScore { get; set; } = 100;
-        
-        [Export] public float PlayerMaxAngularVelocity { get; set; } = 30;
+        [Export] public int WhiskTickScore { get; set; } = 10;
 
         [ExportCategory("Dependencies")]
         [Export]
@@ -27,6 +25,9 @@ namespace Game
         public Bowl bowl;
         [Export] 
         public Label scoreLabel;
+
+        private int lastTick = 0;
+        private int scoreMultiplier = 0;
 
         public void StartGame(PlayerData[] players)
         {
@@ -39,7 +40,23 @@ namespace Game
         public override void _Process(double delta)
         {
             Time += delta;
-            var scoreMultiplier = (int)bowl.TotalAverageAngularVelocity;
+            
+            // if game started
+            scoreMultiplier = (int)bowl.TotalAverageLinearVelocity / 540;
+            if ((int)Time > lastTick)
+            {
+                ScoreTick();
+                lastTick = (int)Time;
+            }
+           
+            if (Time > GameDuration)
+            {
+                EndGame();
+            }
+        }
+
+        private void ScoreTick()
+        {
             foreach(var player in playerManager.Players)
             {
                 if (player.GetWhisk() is not null)
@@ -49,10 +66,6 @@ namespace Game
             }
 
             scoreLabel.Text = "Score: " + Score;
-            if (Time > GameDuration)
-            {
-                EndGame();
-            }
         }
 
         public void EndGame()
